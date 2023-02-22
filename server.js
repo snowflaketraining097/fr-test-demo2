c33c8e5ee93f665a1e0d78bf7eb884e948e7cfb0
snowflaketraining097 <117883392+snowflaketraining097@users.noreply.github.com>
2023-02-22 23:26:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-// server.js
-const http = require('http')
-const PORT = process.env.PORT || 9001
-http
-  .createServer((req, res) => {
-    console.log('New connection')
-    res.end('Hello Cloud Run 2222222222233333333333333')
-  })
-  .listen(PORT, () => console.log('Listening on', PORT))
+const http = require("http");
+const fs = require("fs");
+const port = process.env.port || 9001;
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("index.html", function (error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write("Error: File not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+server.listen(port, function (error) {
+  if (error) {
+    console.log("An error has occured", error);
+  } else {
+    console.log("Server is running on port " + port);
+  }
+});

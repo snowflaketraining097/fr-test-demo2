2ce6a2c01e86d9cf26e5e01af2cab5a599ea22c1
snowflaketraining097 <117883392+snowflaketraining097@users.noreply.github.com>
2023-02-22 21:49:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 const http = require('http')
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 9001
 http
   .createServer((req, res) => {
     console.log('New connection')

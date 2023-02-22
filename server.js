15c319976e4dde01975d6432bb5cfa83b743400c
snowflaketraining097 <117883392+snowflaketraining097@users.noreply.github.com>
2023-02-22 22:36:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 9001
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run')
+    res.end('Hello Cloud Run' + new Date() )
   })
   .listen(PORT, () => console.log('Listening on', PORT))
